ca6a8f5c5e307257b3fe3eff07da140926e3393e
eliodinucci <126082250+eliodinucci@users.noreply.github.com>
2024-01-29 17:20:09 +0100

    Add files via upload
    backend prova e-commerce

---
+++ e-commerce/e-commerce/src/main/java/it/test/ecommerce/entity/HtmlEntity.java
@@ -0,0 +1,61 @@
+package it.test.ecommerce.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class HtmlEntity {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idHtml;
+	@Column
+	private String sigla;
+	@Column
+	private String path;
+	@Column
+	private String file;
+	
+	public HtmlEntity() {
+		
+	}
+	
+	
+	public HtmlEntity(int idHtml, String sigla, String path, String file) {
+		super();
+		this.idHtml = idHtml;
+		this.sigla = sigla;
+		this.path = path;
+		this.file = file;
+	}
+	
+	public int getIdHtml() {
+		return idHtml;
+	}
+	public void setIdHtml(int idHtml) {
+		this.idHtml = idHtml;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+}
